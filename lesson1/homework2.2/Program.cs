28a2d21372387411e9b236c0d4e4a2604ef1737a
usermame <email>
2022-11-22 23:26:08 +0300

    home2

--- lesson1/homework2.2/Program.cs
+++ lesson1/homework2.2/Program.cs
@@ -3,12 +3,34 @@
 78 -> третьей цифры нет
 32679 -> 6*/
 
-Random rnd = new Random();
-int[] Random = {0,End-1};
-Index = 0;
-In
-if(){
-    Console.WriteLine("третьей цифры нет");}
-    else {
-        Console.WriteLine(Random[2]);}
-
+/*int Number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Number);
+if(Number < 100) 
+{
+    Console.WriteLine("Третьей цифры нет");
+    }
+    else
+    {
+        Console.WriteLine($"Третья цифра" {Number[2]});
+    }*/
+Console.WriteLine("Введите число");
+int Number = Convert.ToInt32(Console.ReadLine());
+if(Number <100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+if(Number > 99 & Number < 1000) 
+    {
+       Number = Number % 10;
+        Console.WriteLine(Number);
+    }    
+while (Number > 99)
+    if(Number > 999)
+    {
+        Number = Number / 10;  
+        Console.WriteLine(Number % 10);        
+    }
+    
+    
+     
+    
--- lesson1/homework2.3/Program.cs
+++ lesson1/homework2.3/Program.cs
@@ -7,7 +7,9 @@
 int numberday = new int();
 Console.WriteLine("Введите число от 1 до 7 обозначающее день недели");
 numberday = Convert.ToInt32(Console.ReadLine());
-if(numberday <= 5) {
-    Console.WriteLine("Будний день");
+if(numberday == 6 || numberday == 7) {
+    Console.WriteLine("Выходной день!!!");
 }
-    else {Console.WriteLine("Выходной день!!!");}
+if(numberday > 0 && numberday < 6) {
+    Console.WriteLine("Будний день");}
+
