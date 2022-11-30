a326a5d97e713e751db23bed8b2c2f9a0e1ef435
usermame <email>
2022-11-30 22:16:54 +0300

    hw6

---
+++ lesson1/homework6.1/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Задача 41: Пользователь вводит с клавиатуры M чисел.
+ Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3*/
+
+Console.Write("Пожалуйста, введите число символизирующее количество задаваемых вами чисел: ");
+int NumbersUser = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[NumbersUser];
+
+for (int i = 0; i < NumbersUser; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+int NumbersGreaterZero(int[] array)
+{
+    int numbersGreaterZero = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            numbersGreaterZero++;
+        }
+    }
+    return numbersGreaterZero;
+}
+
+int numbersGreaterZero = NumbersGreaterZero(array);
+Console.WriteLine($"Количество чисел больше 0 = {numbersGreaterZero}");
---
+++ lesson1/homework7.3/Program.cs
@@ -0,0 +1,63 @@
+﻿/*Задача 52. Задайте двумерный массив из целых чисел.
+ Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.*/
+
+int[,] generate2DArray(int lengthRow, int lengthCol, int deviation)
+{
+    int[,] array = new int[lengthRow,lengthCol];
+    for (int i = 0; i < lengthRow; i++)
+    {
+        for (int j = 0; j < lengthCol; j++)
+        {
+            array[i,j] = new Random().Next(-deviation,deviation +1);
+        }
+    }
+    return array;
+}
+
+int ArithmeticMeanOfEachColumn(int[,] array)
+{
+int[] result= new int[array.GetLength(0)];
+for(int i = 0; i < array.GetLength(0); i++)
+    {   int summ = 0;
+        for(int j = 0; j < array.GetLength(0); j++)
+        {
+            summ = summ + array[i,j];
+            result[j] = summ / array.GetLength(0);
+        }
+    }
+return result;
+}
+
+
+void printColor(string information, ConsoleColor color)
+{
+    Console.ForegroundColor = color;
+    Console.Write(information);
+    Console.ResetColor();
+}
+void print2dArray(int[,] array)
+{
+    Console.Write("\t");
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        printColor(i + "\t",ConsoleColor.DarkYellow);
+    }
+    Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        printColor(i+"\t",ConsoleColor.DarkYellow);
+        for (int j = 0; j <  array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] array = generate2DArray(4,3,10);
+print2dArray(array);
+int[] arithmeticMeanOfEachColumn = ArithmeticMeanOfEachColumn(array);
